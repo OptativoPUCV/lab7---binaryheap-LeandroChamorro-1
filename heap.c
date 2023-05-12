a94e75c6a3eaff6ba1115e0dbfe2d838b83afc1d
Replit user <>
2023-05-12 19:13:31 +0000

    new try

--- heap.c
+++ heap.c
@@ -28,37 +28,44 @@ void* heap_top(Heap* pq){
 
 
 void heap_push(Heap* pq, void* data, int priority){
-  if (pq->size == pq->capac){
+  if(pq->size == pq->capac)
+  {
     pq->capac = pq->capac * 2 + 1;
     pq->heapArray = (heapElem*)realloc(pq->heapArray, pq->capac * sizeof(heapElem));
   }
+  
   int currentIndex = pq->size;
-  int parentIndex = (currentIndex -1)/2;
+  int parentIndex = (currentIndex - 1) / 2;
 
-  while (currentIndex >0&& pq->heapArray[parentIndex].priority < priority){
+  while(currentIndex > 0 && pq->heapArray[parentIndex].priority < priority)
+  {
     pq->heapArray[currentIndex] = pq->heapArray[parentIndex];
     currentIndex = parentIndex;
-    parentIndex = (currentIndex - 1)/ 2;
+    parentIndex = (currentIndex - 1) / 2;
   }
 
   pq->heapArray[currentIndex].data = data;
-  pq->heapArray[currentIndex].priority =priority;
+  pq->heapArray[currentIndex].priority = priority;
   pq->size++;
 }
 
 
 void heap_pop(Heap* pq){
-  if(pq->size ==0){
+  if(pq->size == 0)
+  {
     return;
   }
+  
   pq->size--;
-  pq->heapArray[0]= pq->heapArray[pq->size];
+  pq->heapArray[0] = pq->heapArray[pq->size];
   int currentIndex = 0;
-  int childIndex=1 ;
-  while(childIndex< pq->size){
-    int rightChildIndex = childIndex +1;
-    if(rightChildIndex < pq->size && pq->heapArray[rightChildIndex].priority > pq->heapArray[childIndex].priority){
-      childIndex= rightChildIndex;
+  int childIndex = 1;
+  
+  while(childIndex < pq->size){
+    int rightChildIndex = childIndex + 1;
+    if(rightChildIndex < pq->size && pq->heapArray[rightChildIndex].priority > pq->heapArray[childIndex].priority)
+    {
+      childIndex = rightChildIndex;
     }
 
     if(pq->heapArray[currentIndex].priority >= pq->heapArray[childIndex].priority)
@@ -66,19 +73,18 @@ void heap_pop(Heap* pq){
       break;
     }
 
-    heapElem temp= pq->heapArray[currentIndex];
+    heapElem temp = pq->heapArray[currentIndex];
     pq->heapArray[currentIndex] = pq->heapArray[childIndex];
     pq->heapArray[childIndex] = temp;
-    currentIndex= childIndex;
-    childIndex = 2 * currentIndex +1;
-    
+    currentIndex = childIndex;
+    childIndex = 2 * currentIndex + 1;    
   }
 }
 
 Heap* createHeap(){
   Heap* heap = (Heap*)malloc(sizeof(Heap));
-  heap->heapArray = (heapElem*) malloc(3*sizeof(heapElem));
+  heap->heapArray = (heapElem*)malloc(3 * sizeof(heapElem));
   heap->size = 0;
   heap->capac = 3;
-   return heap;
+  return heap;
 }
